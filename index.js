49c63c6f348f74dd75f6a96238c992f794631192
Grace-aloo <graceaaloo@gmail.com>
2022-11-22 16:35:01 +0300

    incomplete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.querySelector('main').remove();
+const newHeader = document.createElement('h1');
+newHeader.bodyNode= 'h1'
+document.body.append(newHeader);
+newHeader.id = ' victory';
+newHeader.innerHTML = "Grace is the champion"
+
