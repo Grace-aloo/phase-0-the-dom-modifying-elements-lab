4dc34266c81ab610d7a0c9fcd15c8ed3971bffff
Grace-aloo <graceaaloo@gmail.com>
2022-11-26 00:04:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Write your code here!
+
 document.querySelector('main').remove();
 const newHeader = document.createElement('h1');
-newHeader.bodyNode= 'h1'
-document.body.append(newHeader);
-newHeader.id = ' victory';
-newHeader.innerHTML = "Grace is the champion"
-
+newHeader.id = 'victory'
+newHeader.innerHTML = 'YOUR-NAME is the champion';
